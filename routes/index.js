2f3b76a3454aeb1802b0902d6654d42fba2128cb
Randa BAlI <baliranda@gmail.com>
2020-06-02 12:07:41 +0200

    debug

--- routes/index.js
+++ routes/index.js
@@ -5,12 +5,8 @@ module.exports = (app) => {
     
     app.use("/deal", require("./deal")(app));
     app.use("/theme", require("./theme")(app));
-<<<<<<< HEAD
-    app.use("/users", require("./users")(app));
-=======
     app.use("/user", require("./user")(app));
     app.use("/commentaire", require("./commentaire")(app));
->>>>>>> b2b65b99604d352065ddf4264e58b4eef9d995a7
 };
 
 
--- routes/users.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-
-module.exports = (app) => {
-    router.get("/", app.controllers.users.getAll)
-    
-    //router.post("/create", app.controllers.users.create);
-
-    return router;
-  };
