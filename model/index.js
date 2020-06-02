ab384c03ce4f88ea3845483b77b69c2220916282
Randa BAlI <baliranda@gmail.com>
2020-06-02 11:07:23 +0200

    add deal.json

--- model/index.js
+++ model/index.js
@@ -2,10 +2,6 @@ module.exports = (app) => {
     app.models = {
         Deal : require("./deal.js"),
         User : require("./user.js"),
-<<<<<<< HEAD
-    
-=======
         Theme : require("./theme.js")
->>>>>>> 91bab70460f4a43ecafa92e5d98575910a9ec093
     };
 }
