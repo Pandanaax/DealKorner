92eeefcc6280e1c1926934ce2bfe841e5c834ede
Randa BAlI <baliranda@gmail.com>
2020-06-02 11:24:25 +0200

    add create to deal

--- routes/deal.js
+++ routes/deal.js
@@ -3,7 +3,7 @@ const router = require("express").Router();
 module.exports = (app) => {
     router.get("/", app.controllers.deal.getAll)
     
-    // router.post("/create", app.controllers.deal.create);
+    router.post("/create", app.controllers.deal.create);
 
     return router;
   };
