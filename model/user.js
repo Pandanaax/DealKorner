8769b3a02d0b4674903d4ea65ce54135aea78822
Hicham <h.kassou91@gmail.com>
2020-06-02 10:06:50 +0200

    ajout du model, du controller et de la route User

---
+++ model/user.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+var Schema = mongoose.Schema;
+
+var userSchema = new Schema({
+
+    pseudo: String,
+    email : String,
+    password: String,
+    urlPhoto : String,
+    dealPoste: [ {
+        type: Schema.Types.ObjectId,
+        ref: "Deal"
+    },
+    ]
+    
+    
+    
+});
+
+
+
+module.exports = mongoose.model("User", userSchema);
--- routes/index.js
+++ routes/index.js
@@ -4,4 +4,5 @@ module.exports = (app) => {
     })
     
     app.use("/deals", require("./deals")(app));
+    app.use("/users", require("./users")(app));
 };
