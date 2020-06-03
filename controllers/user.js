f8e4e39a06651be2c90f641e9b7cab9052688599
Cedric Ktourza <cedric.ktourza@gmail.com>
2020-06-03 12:25:11 +0200

    MAJ de la fonction connexion(Ajout de la fonction Bcrypt)+ MAJ de create

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,7 @@
 module.exports = (app) => {
     const User = app.models.User;
     const jwt = require('jsonwebtoken');
+    const bcrypt = require('bcrypt');
 
     function getAll(req, res) {
         User.find({}, function (err, docs) {
@@ -11,16 +12,33 @@ module.exports = (app) => {
                 }
             }); 
         }
+
     function create(req, res) {
-        let newUser =req.body;
-            User.create(newUser, function(err,docs) {
-                if(err){
-                    res.send(err);
-                }else{
-                    res.send("Le user a bien été crée");
+        const saltRounds = 10;
+        const Password = req.body.password;
+        bcrypt.hash(Password, saltRounds, function(err, hash) {
+            if(err){
+                res.send(err);
+            }else{
+                let passwordHash = hash;
+                let newUser = {
+                    pseudo: req.body.pseudo,
+                    email : req.body.email,
+                    password: passwordHash,
+                    urlPhoto : req.body.urlPhoto,
+                };
+
+                    User.create(newUser, function(err,docs) {
+                        if(err){
+                            res.send(err);
+                        }else{
+                            res.send("Le user a bien été crée");
+                             }
+                    });
                 }
-            });
-        }
+        });
+    }
+
     function createAll(req, res){    
     User.insertMany(req.body, function(err,docs) {
         if(err){
@@ -32,20 +50,44 @@ module.exports = (app) => {
     }
 
     function connect(req, res) {
-        User.findOne({email: req.body.email}, function(err,user) {
-            if(!user){
-                res.status(401).json({ message: "Impossible de se connecter. Mauvais email." }) }
-                else{
-            if (user) {
-              if (req.body.password !== user.password) {
-                res.status(401).json({ message: 'Impossible de se connecter. Mauvais mot de passe.'});
-              } else {
-                return res.json({token: jwt.sign({ email: user.email, id: user.id}, 'Cacher')});
-              }
-             }
+        if (req.body.email) {
+
+            if (req.body.password) {
+                User.findOne({
+                    email: req.body.email
+                }, function (err, user) {
+                    if (err) {
+                        res.send(err + "Une erreur technique est survenue");
+                    }
+                    if (!user) {
+                        res.status(401).json({
+                            message: "Impossible de se connecter. Mauvais email."
+                        })
+                    } else {
+                            let password = req.body.password;
+
+                            bcrypt.compare(password, user.password, function (err, result) {
+                                if (result) {
+                                    res.json({
+                                        token: jwt.sign({
+                                            email: user.email,
+                                            //id: user.id
+                                        }, 'Cacher')
+                                    });
+                                } else {
+                                    res.send(err + "Le mot de passe ne correspond pas");
+                                }
+                            });
+                        }
+                    }
+                );
+            } else {
+                res.send("Aucun mot de passe");
             }
-          });
-        };
-        
+
+        } else {
+            res.send("Aucun mail");
+        }
+    };
     return {getAll,create,createAll,connect};
 }
--- model/user.js
+++ model/user.js
@@ -7,7 +7,7 @@ var userSchema = new Schema({
     pseudo: String,
     email : String,
     password: String,
-    urlPhoto : String,
+    urlPhoto : [String],
     dealPoste: [ {
         type: Schema.Types.ObjectId,
         ref: "Deal"
